25fbda328e279cc81b9e7392321e44b8f3c70f2d
Vlad Turcu <turcuvlad87@gmail.com>
2020-04-11 15:55:40 +0100

    NEW theme first commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,16 @@
+<?php
+// Version: 2.0; Themes
+
+global $scripturl;
+
+$txt['maintenance'] = '(Maintenance)';
+$txt['approval_member'] = 'Approval';
+$txt['open_reports'] = 'Reports';
+$txt['user_replies'] = 'Replies';
+$txt['user_new_posts'] = 'New Posts';
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['google_url'] = 'Google+ URL';
+$txt['rss_url'] = 'RSS URL';
+
+?>
